14725708f78b5b0e95789916df219b0926fc70e5
Matteo <46228228+RampeoMattone@users.noreply.github.com>
2020-12-29 21:58:26 +0100

    router upgrade part 2

--- router.lua
+++ router.lua
@@ -12,7 +12,7 @@ while turtle.suck() do
 		destination = tonumber(io.read())
 	until destination
 	destination = math.floor(destination)
-	routing.write('"' .. item  .. '"' .. "=" .. destination)
+	routing.write(item  .. "=" .. destination .. "\n")
 	turtle.dropUp()
 end
 routing.close()
