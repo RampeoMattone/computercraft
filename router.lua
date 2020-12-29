ed9352526cf1f82fd283aa791162d53b988bb5d3
Matteo <46228228+RampeoMattone@users.noreply.github.com>
2020-12-29 22:07:03 +0100

    router upgrade part 3

--- router.lua
+++ router.lua
@@ -1,10 +1,11 @@
 -- script created by GiappoNylon
 
-local routing = fs.open("filter.dat", "a") -- file where we append all items and destinations
-if not routing then return end
+-- local routing = fs.open("filter.dat", "a") -- file where we append all items and destinations
+-- if not routing then return end
+local routing = {}
 print("type next to each item where you want it to go")
 turtle.select(1)
-while turtle.suck() do
+while turtle.suckUp() do
 	local item = turtle.getItemDetail().name
 	local destination
 	repeat
@@ -12,8 +13,7 @@ while turtle.suck() do
 		destination = tonumber(io.read())
 	until destination
 	destination = math.floor(destination)
-	routing.write(item  .. "=" .. destination .. "\n")
-	turtle.dropUp()
+	table.insert(routing, item, destination)
+	turtle.dropDown()
 end
-routing.close()
-
+for k, v in pairs(routing) do print(k, v) end
