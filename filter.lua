8333ac6dbf3497edcdae675d31cac87e04e394be
Matteo <46228228+RampeoMattone@users.noreply.github.com>
2020-12-29 21:15:06 +0100

    filter

--- filter.lua
+++ filter.lua
@@ -1,18 +1,37 @@
 -- script created by GiappoNylon
--- this is a filter
--- the setup is as follows:
--- left -> output
--- right -> input
--- top -> filter input
--- bottom -> disk
 
-local filter_path = "disk/filter.dat"
-
-local function getFilter()
-	local filter = {} -- the filter is a long list of 
-	os.run(filter, filter_path)
+local routing = {}
+local function getRouting()
+	os.run(filter, "routing.dat")
 end
 
-local function appendFilter(item)
-
+-- scan the inventory to get all item id's and inventory slots
+-- returns 2 tables scan and scan_reverse
+local scan()
+	local scan, scan_reverse = {}, {}
+	for i=1, 16 do
+		scan[i] = turtle.getItemDetail.name
+	end
+	for k,v in pairs(scan) do
+		if not scan_reverse[v] then scan_reverse[v] = {k}
+		else table.insert(scan_reverse[v], k)
+		end
+	end
+	return scan, scan_reverse
 end
+
+-- returns true if and only if the first item is closer to the starting point than the second
+local function compare(i1, i2)
+	if routing[i1][1] < routing[i2][1] then return true
+	elseif routing[i1][1] == routing[i2][1] then return routing[i1][2] < routing[i2][2]
+	else return false
+end
+
+-- generate a "route" i.e. generate an ordered list of items
+local function route()
+	local route, reverse = scan() -- route will be ordered from nearest to furthest from the turtle. reverse is a reverse table to match item id's with inv. slots
+	table.sort(route, compare)
+	return route
+end
+
+print(route())
