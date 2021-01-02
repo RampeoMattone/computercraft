6451292422861db695a4b14ab5c90083787f2bf9
Matteo <46228228+RampeoMattone@users.noreply.github.com>
2021-01-02 18:49:02 +0100

    refractor

--- sorting/filter.lua
+++ sorting/filter.lua
@@ -1,5 +1,39 @@
 -- script created by GiappoNylon
---
+
+local function bulk()
+	local specific = {}
+	if fs.exists("disk/routing.dat") then
+		local file = fs.open("disk/routing.dat", "r")
+		specific = textutils.unserialise(file.readAll())
+		file.close()
+	end
+	turtle.select(1)
+	print("type where you want all these items to go:")
+	local destination
+	repeat -- ask the user where to put the item on sort
+			destination = tonumber(io.read())
+	until destination
+	while turtle.suckDown() do -- get an item from the chest
+		local id = turtle.getItemDetail().name -- get the item name
+		local mod, item = string.match(id, "(.+):(.+)")
+		local destination -- mod name, item name, place to put the item
+		local mod, item
+		destination = math.floor(destination) -- make sure the number we received is an integer
+		if not specific[mod] then -- generate an entry for the mod if it does not already exist
+			specific[mod] = {}
+		end
+		specific[mod][item] = destination -- assign the item to a destination
+		turtle.dropUp() -- release the item
+	end
+	local json = textutils.serialise(specific)
+	local file = fs.open("disk/routing.dat", "w") -- file where we append all items and destinations
+	if not file then return end
+	-- serialize the routing table
+	file.write(json)
+	file.close()
+end
+
+
 local function specific()
 	local specific = {}
 	if fs.exists("disk/routing.dat") then
@@ -10,12 +44,11 @@ local function specific()
 	
 	turtle.select(1)
 	print("type next to each item where you want it to go")
-	while turtle.suckUp() do -- get an item from the chest
+	while turtle.suckDown() do -- get an item from the chest
 		local id = turtle.getItemDetail().name -- get the item name
 		local destination -- mod name, item name, place to put the item
-		local mod, item
+		local mod, item = string.match(id, "(.+):(.+)")
 		repeat -- ask the user where to put the item on sort
-			mod, item = string.match(id, "(.+):(.+)")
 			print(mod, item, "will be sent to:")
 			destination = tonumber(io.read())
 		until destination
@@ -24,7 +57,7 @@ local function specific()
 			specific[mod] = {}
 		end
 		specific[mod][item] = destination -- assign the item to a destination
-		turtle.dropDown() -- release the item
+		turtle.dropUp() -- release the item
 	end
 	local json = textutils.serialise(specific)
 	local file = fs.open("disk/routing.dat", "w") -- file where we append all items and destinations
@@ -45,18 +78,17 @@ local function wildcards()
 	--os.run(wildcards, "disk/wildcards.dat") -- load the filter that has already been written to disk
 	turtle.select(1)
 	print("type next to each mod where you want it to go")
-	while turtle.suckUp() do -- get an item from the chest
+	while turtle.suckDown() do -- get an item from the chest
 		local id = turtle.getItemDetail().name -- get the item name
 		local destination -- mod name, item name, place to put the item
-		local mod
+		local mod = string.match(id, "(.+):.+")
 		repeat -- ask the user where to put the item on sort
-			mod = string.match(id, "(.+):.+")
 			print(mod, "will be sent to:")
 			destination = tonumber(io.read())
 		until destination
 		destination = math.floor(destination) -- make sure the number we received is an integer
 		wildcards[mod] = destination -- assign the mod to a destination
-		turtle.dropDown() -- release the item
+		turtle.dropUp() -- release the item
 	end
 	local json = textutils.serialise(wildcards)
 	local file = fs.open("disk/wildcards.dat", "w") -- file where we append all items and destinations
@@ -67,9 +99,9 @@ end
 
 local choice
 repeat --ask the user what he wants to set up (wildcards/specific routes)
-	print("type W to define wildcards or S to define specific routes")
+	print("type W to define wildcards, S to define specific routes for each item or B to define specific routes for every item in the bottom chest")
 	choice = io.read():lower()
-until choice == "w" or choice == "s"
+until choice == "w" or choice == "s" or choice == "b"
 if choice == "w" then wildcards()
 else specific()
 end
